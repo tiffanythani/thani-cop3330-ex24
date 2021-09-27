b196e1e19ae618ab3f54e1db56bbe53b98a8bea1
Tiffany Thani <tiffanythani18@gmail.com>
2021-09-27 16:04:08 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Tiffany Thani
+ */
+
+
+ public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
